4e4a0df300d57962f60e1839d3d9b4c5bed2a7d7
Davi Macedo <macedodavi27@icloud.com>
2026-03-31 12:51:44 -0300

    Aula 9 - Comentários e Boas Práticas

--- src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,16 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
 public class OlaDevDojo {
+    // Comentário em Linha
+
+    /*
+    Comentário de múltiplas linhas
+    muitas linhas!!
+     */
+
     public static void main(String[] args){
         System.out.println("Olá Mundo!");
     }
