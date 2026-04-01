b358ca3a1b53b2f569c8a996f88252466661229a
Davi Macedo <macedodavi27@icloud.com>
2026-04-01 12:36:24 -0300

    Aula 14 - Tipos Primitivos: Exercício, prática e debug

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicios.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+        String nome = "Davi Macedo";
+        String endereco = "Rua dos Canedos, N° 100, Bairro Dalila, São Paulo SP";
+        float salario = 10350.50F;
+        String data = "01/04/2026";
+
+        System.out.println("Eu " + nome + ", morando no endereço: " + endereco + ", confirmo que recebi o salário de R$" + salario + ", na data: " + data);
+    }
+}
