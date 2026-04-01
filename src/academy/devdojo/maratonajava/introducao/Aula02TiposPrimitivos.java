1b646764c80ff131c4e830e9ab611a9e490608b8
Davi Macedo <macedodavi27@icloud.com>
2026-04-01 10:03:21 -0300

    Aula 10 - Tipos Primitivos: Convenções de Variáveis

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // Temos 8 tipos primitivos:
+            //int, double, float, char, byte, short, long, boolean
+
+        int age = 10;
+
+        System.out.println("a idade é " + age + " anos");
+    }
+}
