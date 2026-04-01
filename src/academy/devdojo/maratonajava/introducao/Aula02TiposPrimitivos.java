bbdb283d34b8054c59aece82e6985796688003e7
Davi Macedo <macedodavi27@icloud.com>
2026-04-01 10:27:22 -0300

    Aula 12 - Tipos Primitivos: Casting

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -5,12 +5,12 @@ public class Aula02TiposPrimitivos {
         // Temos 8 tipos primitivos:
             //int, double, float, char, byte, short, long, boolean
 
-        int age = 10;
-        long numeroGrande = 100000000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
-        byte idadeByte = 10;
-        short idadeShort = 10;
+        int age = (int) 1000000000000L;
+        long numeroGrande = 100000000L;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = 2500.0F;
+        byte idadeByte = 127;
+        short idadeShort = 32000;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = 87;
