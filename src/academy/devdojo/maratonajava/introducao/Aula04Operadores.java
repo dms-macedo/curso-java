5bd650358d3c3ad0b4e0d6eb83bec0f37d227074
Davi Macedo <macedodavi27@icloud.com>
2026-04-05 00:35:56 -0300

    Aula 16 - Operadores Lógicos e Resto

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -4,10 +4,28 @@ public class Aula04Operadores {
     public static void main(String[] args) {
         // Operadores Aritméticos
         // + - / *
+        // % resto da divisão
         int numero1 = 10;
         int numero2 = 20;
         int resultado = numero1 + numero2;
         System.out.println(resultado);
+
+        int resto = 20 % 2;
+        System.out.println(resto);
+
+        // Operadores Relacionais
+        // < > <= >= == != (Sempre retornam true ou false)
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println("isDezMaiorQueVinte: " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte: " + isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte: " + isDezIgualVinte);
+        System.out.println("isDezIgualDez: " + isDezIgualDez);
+        System.out.println("isDezDiferenteDez: " + isDezDiferenteDez);
+
     }
 
 }
