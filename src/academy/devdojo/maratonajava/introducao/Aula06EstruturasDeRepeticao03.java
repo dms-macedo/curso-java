8c434ad92ba4716b333fa6e24ebb346b987034d8
Davi Macedo <macedodavi27@icloud.com>
2026-04-06 13:54:11 -0300

    Aula 29 - Estruturas de Repetição (Break)

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao03.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao03 {
+    public static void main(String[] args) {
+        //Imprimir os primeiros 25 numeros de um dado valor
+
+        int valorMax = 50;
+        for (int i = 0; i <= valorMax; i++) {
+            if (i > 25) {
+                break;
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
