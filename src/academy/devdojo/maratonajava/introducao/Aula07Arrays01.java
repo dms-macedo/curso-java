e301ddec6160613dcda1f7e6c21ca6c4676e5b87
Davi Macedo <macedodavi27@icloud.com>
2026-04-07 12:22:38 -0300

    Aula 33 - Arrays Parte 2

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -3,6 +3,9 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula07Arrays01 {
     public static void main(String[] args) {
         int[] idades = new int[3]; // é considerao um objeto na memória (new int)
+
+        // Arays são objetos
+
         idades[0] = 21;
         idades[1] = 15;
         idades[2] = 11;
---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        // Padrões de inicialização:
+            // byte, short, int, long, float e double => 0
+            // char => '\u0000'
+            // boolean => false
+            // String => null
+
+        char[] nomes = new char[3];
+        System.out.println(nomes[0]);
+        System.out.println(nomes[1]);
+        System.out.println(nomes[2]);
+
+
+    }
+}
