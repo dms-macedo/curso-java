f3bbe61ebf60568d755c43a2c360da0f207e710c
Davi Macedo <macedodavi27@icloud.com>
2026-04-12 14:47:39 -0300

    Aula 40 - Orientação a Objetos (Introdução classes pt2)

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/Estudantetest02.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class Estudantetest02 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante.idade);
+
+        Estudante estudante2 = new Estudante();
+
+        System.out.println("========================");
+
+        estudante2.nome = "Naruto";
+        System.out.println(estudante2.nome);
+        System.out.println(estudante2.sexo);
+        System.out.println(estudante2.idade);
+
+    }
+}
