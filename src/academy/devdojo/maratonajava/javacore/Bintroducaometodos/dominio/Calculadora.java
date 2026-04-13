9cb7c4207832502a786d9e2e110af856c7d0e379
Davi Macedo <macedodavi27@icloud.com>
2026-04-12 21:32:58 -0300

    Aula 46 -  Orientação a Objetos (Métodos Parte 3 - Retorno Parte 1)

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -12,4 +12,13 @@ public class Calculadora {
     public void multiplicaDoisNumeros(double num1, double num2){
         System.out.println(num1 * num2);
     }
+
+    //Com retorno
+    public double divideDoisNumeros(double num1, double num2){
+        if (num2 == 0) {
+            return 0;
+        }
+
+        return num1/num2;
+    }
 }
