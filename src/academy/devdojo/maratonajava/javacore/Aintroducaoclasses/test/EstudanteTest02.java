4717f6a8f3c6740094023fd4839c3b840621a12c
Davi Macedo <macedodavi27@icloud.com>
2026-04-15 12:03:29 -0300

    Mudança de Nome de Arquivo - EstudanteTest02

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/EstudanteTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/EstudanteTest02.java
@@ -2,7 +2,7 @@ package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
 
 import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
 
-public class Estudantetest02 {
+public class EstudanteTest02 {
     public static void main(String[] args) {
         Estudante estudante = new Estudante();
 
