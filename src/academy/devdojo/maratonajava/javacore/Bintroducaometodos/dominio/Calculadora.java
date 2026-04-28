4a2e6d5b4191a0f98f41d3504250d6a853809e1b
Davi Macedo <macedodavi27@icloud.com>
2026-04-27 23:54:10 -0300

    Aula 52 - Orientação a Objetos (Métodos Parte  9 - VarArgs)

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -47,4 +47,20 @@ public class Calculadora {
         System.out.println("Primeiro Número: " + num1);
         System.out.println("Segundo Número: " + num2);
     }
+
+    public void somaArray(int[] numeros) {
+        int soma = 0;
+        for(int num: numeros) {
+            soma += num;
+        }
+        System.out.println(soma);
+    }
+
+    public void somaVarArgs(int... numeros) {
+        int soma = 0;
+        for(int num: numeros) {
+            soma += num;
+        }
+        System.out.println(soma);
+    }
 }
