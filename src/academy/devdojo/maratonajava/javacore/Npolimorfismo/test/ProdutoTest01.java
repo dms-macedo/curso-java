5ff99b6d7f187feb325621743533c9cdaf794fe0
Davi Macedo <macedodavi27@icloud.com>
2026-06-02 12:32:17 -0300

    Aula 90 - Orientação a Objetos (Polimorfismo Parte 1 - Introdução)

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
+
+public class ProdutoTest01 {
+    public static void main(String[] args) {
+        Computador computador = new Computador("ASUS x11", 2000);
+        Tomate tomate = new Tomate("SISISI", 10);
+        CalculadoraImposto.calcularImpostoComputador(computador);
+        System.out.println("-----------------------------------------------------");
+        CalculadoraImposto.calcularImpostoTomate(tomate);
+    }
+}
