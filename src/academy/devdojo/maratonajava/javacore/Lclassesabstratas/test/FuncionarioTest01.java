172827950329ff66fa0c734711366fb8a3f0c431
Davi Macedo <macedodavi27@icloud.com>
2026-06-01 21:37:34 -0300

    Aula 84 - Orientação a Objetos (Classes Abstratas Parte 1)

---
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Lclassesabstratas.test;
+
+import academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio.Desenvolvedor;
+import academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio.Gerente;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente("Nani", 7700);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Touya", 10000);
+        System.out.println(gerente);
+        System.out.println(desenvolvedor);
+    }
+}
