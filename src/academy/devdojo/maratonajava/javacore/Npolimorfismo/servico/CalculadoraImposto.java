6ead68287aa4f28ed4baa16f766770ca6cd0e872
Davi Macedo <macedodavi27@icloud.com>
2026-06-03 13:52:08 -0300

    Aula 93 - Orientação a Objetos (Polimorfismo Parte 4 - Cast e Instanceof)

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
@@ -12,6 +12,10 @@ public class CalculadoraImposto {
         System.out.println("=> Produto: " + produto.getNome());
         System.out.println("=> Valor: " + produto.getValor());
         System.out.println("=> Imposto a ser pago: " + imposto);
+
+        if (produto instanceof Tomate) {
+            System.out.println(((Tomate) produto).getDataValidade());
+        }
     }
 
 }
