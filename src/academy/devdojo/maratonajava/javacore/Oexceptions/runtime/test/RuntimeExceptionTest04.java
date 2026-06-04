491e45289c5b7df292b2cd80813ccba4b64ecdb5
Davi Macedo <macedodavi27@icloud.com>
2026-06-04 17:57:03 -0300

    Aula 102 - Exceções Parte 8 (Multi catch em linha)

--- src/academy/devdojo/maratonajava/javacore/Oexceptions/runtime/test/RuntimeExceptionTest04.java
+++ src/academy/devdojo/maratonajava/javacore/Oexceptions/runtime/test/RuntimeExceptionTest04.java
@@ -8,24 +8,18 @@ public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
         try {
             throw new IndexOutOfBoundsException();
-        } catch (ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e){
             System.out.println("Dentro do ArrayIndexOutOfBoundsException");
         } catch (IndexOutOfBoundsException e){
             System.out.println("Dentro do IndexOutOfBoundsException");
-        } catch (IllegalArgumentException e){
-            System.out.println("Dentro do IllegalArgumentException");
-        } catch (ArithmeticException e){
-            System.out.println("Dentro do ArithmeticException");
         } catch (RuntimeException e){
             System.out.println("Dentro do RuntimeException");
         }
 
         try {
             talvezLanceException();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
