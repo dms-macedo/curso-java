bf78a82462063eda4fbe1dccb5bf52afcfba89ff
Davi Macedo <macedodavi27@icloud.com>
2026-08-23 15:58:30 -0300

    Aula 108 - Classes Utilitárias (Strings Parte 1)

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringTest01 {
+    public static void main(String[] args) {
+        String nome = "William"; //String constant pool
+        String nome2 = "William";
+
+        nome = nome.concat(" Suane");
+        System.out.println(nome);
+        System.out.println(nome == nome2);
+
+        String nome3 = new String("William");
+        System.out.println(nome2 == nome3.intern());
+    }
+}
