1bdadfaaac71359b5a70e4efa537a030a5186d72
Davi Macedo <macedodavi27@icloud.com>
2026-08-24 09:57:03 -0300

    Aula 112 - Classes Utilitárias (Date)

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/DateTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date data = new Date(1787576149062L);
+        System.out.println(data);
+    }
+}
