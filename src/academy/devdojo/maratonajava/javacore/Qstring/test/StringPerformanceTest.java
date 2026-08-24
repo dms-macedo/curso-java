911b50d24427a5b9bb2cd3e057c84ffc59a0c029
Davi Macedo <macedodavi27@icloud.com>
2026-08-24 09:06:03 -0300

    Aula 110 - Classes Utilitárias (String Parte 3 - Desempenho)

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringPerformanceTest.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringPerformanceTest {
+    public static void main(String[] args) {
+        long inicio = System.currentTimeMillis();
+        concatString(30000);
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo de execução: " + (fim - inicio) + "ms");
+
+
+        inicio = System.currentTimeMillis();
+        concatStringBuilder(1000000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo de execução StringBuilder: " + (fim - inicio) + "ms");
+    }
+
+    private static void concatString(int tamanho){
+        String texto = "";
+        for (int i = 0; i < tamanho; i++) {
+            texto += i;
+        }
+    }
+
+    private static void concatStringBuilder(int tamanho){
+        StringBuilder sb = new StringBuilder(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+        }
+    }
+}
