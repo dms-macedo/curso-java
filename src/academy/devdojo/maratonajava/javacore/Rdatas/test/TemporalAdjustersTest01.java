7d0f050160595939ea1c8b2bc310a5676ae48c30
Davi Macedo <macedodavi27@icloud.com>
2026-09-07 15:40:34 -0300

    Aula 126 - Classes Utilitárias (TemporalAdjusters)

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/TemporalAdjustersTest01.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAdjusters;
+
+public class TemporalAdjustersTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = now.with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = now.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = now.with(TemporalAdjusters.previous(DayOfWeek.MONDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+    }
+}
