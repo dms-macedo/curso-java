bf0601afa46353c7d5234e04e2f3ecd3cb61ca7e
Davi Macedo <macedodavi27@icloud.com>
2026-09-08 15:41:11 -0300

    Aula 134 - Classes Utilitárias (Regex Parte 4 - Pattern e Matcher (Quantificadores Parte 1))

---
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/RegularExpressionTest04.java
@@ -0,0 +1,40 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegularExpressionTest04 {
+    public static void main(String[] args) {
+        /*
+            \d = todos os dígitos
+            \D = tudo o que não for digito
+            \s = todos os espaços em branco \t \n \f \r
+            \S = todos os caracteres excluindo os brancos
+            \w = a-Z, A-Z, dígitos, _
+            \W = tudo que não for incluído no \w
+            [] = os caracteres que estiverem dentro de colchetes serão procurados.
+            ? = zero ou uma ocorrência
+            * = zero, ou mais ocorrências
+            + = uma, ou mais ocorrências
+            {n, m} = de n até m ocorrências
+            () = agrupamento
+            | = OU
+            $ = fim da linha
+         */
+        String regex = "0[xX][0-9a-fA-F]+(\\s|$)";
+        String texto1 = "12 0x 0X 0xFFABC 0x10G 0x1";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto1);
+
+        System.out.println("texto:                                      " + texto1);
+        System.out.println("índice:                                     0123456789");
+        System.out.println("regex:                                      " + regex);
+        System.out.print("Posições Encontradas:                       ");
+        while (matcher.find()){
+            System.out.print("< " + matcher.start() + " | " + matcher.group() + " > ");
+        }
+
+        int numeroHexaDecimal = 0x44AA200;
+    }
+}
