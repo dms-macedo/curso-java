5a9e7901a601fda39ccc954192c99f7ad0816fd6
Davi Macedo <macedodavi27@icloud.com>
2026-09-11 13:31:42 -0300

    Aula 146 - Classes Utilitárias (NIO Parte 3 - Nromalização)

--- src/academy/devdojo/maratonajava/javacore/Wnio/test/PathTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/PathTest02.java
@@ -4,11 +4,26 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class PathTest02 {
     public static void main(String[] args) throws IOException {
-        Path subpasta = Paths.get("pasta/subpasta/subsubpasta");
-        Path subpastaCreated = Files.createDirectories(subpasta);
+        Path pasta = Paths.get("pasta");
 
+        if (Files.notExists(pasta)){
+            Files.createDirectory(pasta);
+        }
+
+        Path subPastaPath = Paths.get("pasta/subpasta/subsubpasta");
+        Path subPastaPathCreated = Files.createDirectories(subPastaPath);
+        Path filePath = Paths.get(subPastaPath.toString(), "file.txt");
+
+        if(Files.notExists(filePath)){
+            Files.createFile(filePath);
+        }
+
+        Path source = filePath;
+        Path target = Paths.get(source.getParent().toString(), "file_renamed.txt");
+        Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
     }
 }
