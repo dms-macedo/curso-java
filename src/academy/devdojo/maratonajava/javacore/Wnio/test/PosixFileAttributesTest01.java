cfb75c82c80579ff5571d31acf9a59cd9f6fc18c
Davi Macedo <macedodavi27@icloud.com>
2026-09-17 11:25:12 -0300

    Aula 152 - Classes Utilitárias (NIO Parte 9 - PosixFileAttributes)

---
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/PosixFileAttributesTest01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFileAttributeView;
+import java.nio.file.attribute.PosixFileAttributes;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
+
+public class PosixFileAttributesTest01 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("home/davi/dev/teste2linux.txt");
+        if(Files.notExists(path)) Files.createFile(path);
+        PosixFileAttributes posixFileAttributes = Files.readAttributes(path, PosixFileAttributes.class);
+        System.out.println(posixFileAttributes.permissions());
+        PosixFileAttributeView fileAttributeView = Files.getFileAttributeView(path, PosixFileAttributeView.class);
+        Set<PosixFilePermission> posixFilePermissions = PosixFilePermissions.fromString("");
+        fileAttributeView.setPermissions(posixFilePermissions);
+        System.out.println(fileAttributeView.readAttributes().permissions());
+    }
+}
