afdc92cadab30ac0d0e9a1bddf972ec14142d24d
Davi Macedo <macedodavi27@icloud.com>
2026-09-19 14:59:46 -0300

    Aula 162 - Coleções Parte 2 (Equals Parte 2)

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTest01.java
@@ -5,7 +5,7 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.SmartPhone;
 public class EqualsTest01 {
     public static void main(String[] args) {
         SmartPhone s1 = new SmartPhone("1k1k1k1", "Iphone");
-        SmartPhone s2 = s1;
+        SmartPhone s2 = new SmartPhone("1k1k1k1", "Pixel");
 
         System.out.println(s1.equals(s2));
     }
