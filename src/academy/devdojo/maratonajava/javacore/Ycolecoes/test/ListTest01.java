9c673c89ec0c9252ba520e585966b8d92881cb0d
Davi Macedo <macedodavi27@icloud.com>
2026-09-20 14:09:52 -0300

    Aula 167 - Coleções Parte 7 (List Parte 2)

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -32,5 +32,48 @@ public class ListTest01 {
         for (int i = 0; i < lista.size(); i++){
             System.out.println(lista.get(i));
         }
+
+        // List não aceita tipos primitivos, pois puxa equals e hashCode internamente. Aceita apenas objetos.
+        System.out.println();
+        System.out.println("------ Usando .remove() ------");
+        System.out.println();
+
+        List<String> lista2 = new ArrayList<>();
+        lista2.add("William");
+        lista2.add("Suane");
+        lista2.add("Davi");
+
+        for (String nome : lista2){
+            System.out.println(nome);
+        }
+
+        //aplicando .remove():
+        System.out.println("---------");
+        lista2.remove(1); // por index, remove o Suane
+        for (String nome : lista2){
+            System.out.println(nome);
+        }
+
+        System.out.println("---------");
+        lista2.remove("Davi"); // por objeto, remove o Exato "Davi" que tiver na list (a list usa o equals para ver o exato objeto pedido para remover).
+        for (String nome : lista2){
+            System.out.println(nome);
+        }
+
+        // adicionar um list no outro:
+
+        List<String> nomes = new ArrayList<>();
+        List<String> nomes2 = new ArrayList<>();
+
+        nomes.add("Davi");
+        nomes.add("Macedo");
+        nomes2.add("William");
+        nomes2.add("Suane");
+
+        //adicionar todos os valores String de nomes2 á nomes, com .addAll();
+
+        nomes.addAll(nomes2);
+
+        System.out.println(nomes);
     }
 }
