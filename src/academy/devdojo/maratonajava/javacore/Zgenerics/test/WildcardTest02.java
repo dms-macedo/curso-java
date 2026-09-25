2aa31637a6854eb8f97b791112566cd51e564590
Davi Macedo <macedodavi27@icloud.com>
2026-09-25 11:03:23 -0300

    Aula 185 - Coleções Parte 3 (Wildcard Parte 2)

--- src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildcardTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildcardTest02.java
@@ -13,13 +13,27 @@ public class WildcardTest02 {
         // printConsulta(gatos) <- dá erro, pois não é um List<Animal>
 
         //Por isso o Wildcard (O coringa é importante).
+        // o Wildcard é representado pelo ?.
+        //Para por exemplo passarmos que uma Lista Animal pode aceitar qualquer tipo que extenda Animal usamos <? extends Animal>, veja:
+        //Mas usando esta sintaxe, você perde a possibilidade de adicionar coisas a Lista. pois o java não consegue garantir o tipo exato que a Lista possui em tempo de execução.
+
+        printConsulta(cachorros);
+        printConsulta(gatos); // agora fica tudo certo, pois como passado no parâmetro do método, a lista aceita qualquer tipo que extenda animal.x
+
+        //Para adicionar dados, use o super (<? super Animal>), o super garante que o tipo passado é o tipo ou um tipo maior (Object no caso), para um <? super Cachorro>, ele aceita Animal e Object. Isso permite adicionar valores.
+
+
     }
 
-    private static void printConsulta(List<Animal> animais){ // Só aceita List<Animal>, não aceita filhas.
+    private static void printConsulta(List<? extends Animal> animais){ // Só aceita List<Animal>, não aceita filhas.
         for (Animal animal : animais){
             animal.consultar();
         }
 
         // animais[1] = new Gato(); <- Isso da uma exceção ArrayStoreException, pois quando este código é executado pra um array de cachorros por exemplo, não é possivel colocar na posição 1, um gato. Isso seria possivel se criasse um array de Animais (Animal[] animais) e adicona-se cachorros e gatos, já que eles extendem de Animal. (Vá para WildcardTest02).
+    }
+
+    private static void printConsultaAnimal(List<? super Animal> animais){ // Passa pois Animal aceita seus filhos.
+        animais.add(new Cachorro());
     }
 }
